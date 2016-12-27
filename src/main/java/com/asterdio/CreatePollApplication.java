d000fe4fbd2483d3df20fbd8e31528c7da92e2f5
pravinsubedi <mepravinsubedi@gmail.com>
2016-12-27 11:47:55 +0545

    initial setup

---
+++ src/main/java/com/asterdio/CreatePollApplication.java
@@ -0,0 +1,12 @@
+package com.asterdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatePollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatePollApplication.class, args);
+	}
+}
